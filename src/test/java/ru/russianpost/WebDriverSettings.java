45b845f7eb332e3ca79db57e1995ba8e7546b947
Alex <61654925+LAV007@users.noreply.github.com>
2020-06-30 19:52:02 +0300

    first commit

---
+++ src/test/java/ru/russianpost/WebDriverSettings.java
@@ -0,0 +1,24 @@
+package ru.russianpost;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Класс предназначен для ...
+ */
+public class WebDriverSettings {
+
+    protected ChromeDriver driver;
+
+    @Before
+    public  void setUp() {
+        System.setProperty("webdriver.chrome.driver", "F:\\IdeaProjects3\\untitled\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void close() {
+        driver.quit(); //закрытие браузера
+    }
+}
